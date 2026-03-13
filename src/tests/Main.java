b3a2622fa1a85ab8e116c13af43c36d8751515fa
Clément <clement.bourdier@yahoo.fr>
2026-03-13 09:55:38 +0100

    main - ajout de comptes

--- src/tests/Main.java
+++ src/tests/Main.java
@@ -1,14 +1,21 @@
 package tests;
+import components.Account;
 //1.1.2 Creation of main class for tests
-import components.Client; 
-import java.util.Arrays; 
+import components.Client;
+import components.CurrentAccount;
+import components.SavingsAccount;
 
+import java.util.Arrays; 
+//1.1.2 Creation of main class for tests
 
 public class Main {
     public static void main(String[] args) {
         Client[] tableauClients;
         tableauClients = generateClients(3);
         displayClients(tableauClients);
+      //1.2.3 Creation of the table account
+        Account[] monTableauDeComptes = generateAccounts(tableauClients);
+        displayAccounts(monTableauDeComptes);
     }
 
     /**
@@ -26,6 +33,20 @@ public class Main {
         
         return clients;
     }
+    /**
+     * genere un jeu d'essai d'accounts, à partir d'une liste de clients
+     */
+    public static Account[] generateAccounts(Client[] clients) {
+        //les comptes Current et les comptes savings sont dans le meme tableau
+    	// 2 comptes par clients, donc taille x2
+        Account[] accounts = new Account[clients.length * 2];
+        for (int i = 0; i < clients.length; i++) {
+            accounts[i * 2] = new CurrentAccount("Compte courant", clients[i]);
+            accounts[i * 2 + 1] = new SavingsAccount("Compte Epargne", clients[i]);
+        }
+        
+        return accounts;
+    }
 
     /**
      * affiche les clients en utilisant Stream
@@ -35,4 +56,13 @@ public class Main {
 
         Arrays.stream(clients).forEach(client -> System.out.println(client.toString()));
     }
+    /**
+     * affiche les accounts en utilisant Stream
+     */
+    public static void displayAccounts(Account[] accounts) {
+        System.out.println("\n--- Liste comptes ---");
+        Arrays.stream(accounts).forEach(account -> System.out.println(account.toString()));
+    }
+    
+    
 }
